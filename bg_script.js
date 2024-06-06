2d7eb9f0d3c7c8214df764ec9fdeb384c9c3a630
milesng <miles.c.y.ng@gmail.com>
2024-06-06 19:49:50 +1000

    remove need for www. prefix on hostname

--- bg_script.js
+++ bg_script.js
@@ -2,12 +2,11 @@ setInterval(() => {
     var cur = window.location.hostname;
     chrome.storage.sync.get(["urls", "enabled"], (items) => {
         var url_list = items.urls.replace(/\s+/g, '').split(",");
-        url_list.forEach((url) => console.log(url));
 
         var enabled = items.enabled;
 
         url_list.forEach((url) => {
-            if (url === cur && enabled === "true") {
+            if (url.replace("www.", "") === cur.replace("www.", "") && enabled === "true") {
                 console.log("blocked");
                 window.location.replace("https://tenor.com/en-GB/view/linus-lost-crying-smoke-fog-gif-9298551");
             }
